60cace94d4cc2ea93e14a1309f31dd24deb13f9a
hoangtnm <minhhoangtrannhat@gmail.com>
2019-11-12 12:04:43 +0700

    add predict_camera.py

---
+++ labs/session_09/predict_camera.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import cv2
+import torch
+import numpy as np
+
+from utils import get_net
+from utils import get_device
+from utils import get_metadata
+from utils import predict
+
+
+if __name__ == '__main__':
+    device = get_device()
+
+    # Training dataset metadata
+    _, class_names, class_to_idx = get_metadata(sys.argv[1])
+    num_classes = len(class_names)
+    idx_to_class = {value: key for key, value in class_to_idx.items()}
+
+    # Net initialization
+    net = get_net(classes=num_classes)
+    # checkpoint_dict = torch.load(os.path.join('checkpoint', 'checkpoint.pth'))
+    # net.load_state_dict(checkpoint_dict['model_state_dict'])
+    net.eval()
+    net.to(device)
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, frame = cap.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Prediction
+        output = predict(net, frame)
+        target_idx = torch.argmax(output).item()
+        target_name = idx_to_class[target_idx]
+        print(f'Result: {target_name}')
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
